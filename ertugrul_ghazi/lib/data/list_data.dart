bb8395ab93b8ca70620be598595ae0efa843cc8e
Hafiz M Huzaifa Khalid <66369515+huzaifa215@users.noreply.github.com>
2021-09-28 02:21:30 +0500

    adding images and create List data

---
+++ ertugrul_ghazi/lib/data/list_data.dart
@@ -0,0 +1,88 @@
+List category_names = [
+  {
+    "name": "Ertugrul Ghazi",
+    "image": "images/Categories Images/(1).jpg",
+  },
+  {
+    "name": "Season 1",
+    "image": "images/Categories Images/(2).jpg",
+  },
+  {
+    "name": "Season 2",
+    "image": "images/Categories Images/(3).jpg",
+  },
+  {
+    "name": "Season 3",
+    "image": "images/Categories Images/(4).jpg",
+  },
+  {
+    "name": "Season 4",
+    "image": "images/Categories Images/(5).jpg",
+  },
+];
+List Ertugrul_ghazi = [
+  "images/Ertugrul Ghazi/(1).jpg",
+  "images/Ertugrul Ghazi/(2).jpg",
+  "images/Ertugrul Ghazi/(3).jpg",
+  "images/Ertugrul Ghazi/(4).jpg",
+  "images/Ertugrul Ghazi/(5).jpg",
+  "images/Ertugrul Ghazi/(6).jpg",
+  "images/Ertugrul Ghazi/(7).jpg",
+  "images/Ertugrul Ghazi/(8).jpg",
+  "images/Ertugrul Ghazi/(9).jpg",
+  "images/Ertugrul Ghazi/(10).jpg",
+  "images/Ertugrul Ghazi/(11).jpg",
+  "images/Ertugrul Ghazi/(12).jpg",
+  "images/Ertugrul Ghazi/(13).jpg",
+];
+List Season_1 = [
+  "images/Season 1/(1).jpg",
+  "images/Season 1/(2).jpg",
+  "images/Season 1/(3).jpg",
+  "images/Season 1/(4).jpg",
+  "images/Season 1/(5).jpg",
+  "images/Season 1/(6).jpg",
+  "images/Season 1/(7).jpg",
+  "images/Season 1/(8).jpg",
+  "images/Season 1/(9).jpg",
+  "images/Season 1/(10).jpg",
+  "images/Season 1/(11).jpg",
+];
+List Season_2 = [
+  "images/Season 2/(1).jpg",
+  "images/Season 2/(2).jpg",
+  "images/Season 2/(3).jpg",
+  "images/Season 2/(4).jpg",
+  "images/Season 2/(5).jpg",
+  "images/Season 2/(6).jpg",
+  "images/Season 2/(7).jpg",
+  "images/Season 2/(8).jpg",
+  "images/Season 2/(9).jpg",
+  "images/Season 2/(10).jpg",
+  "images/Season 2/(11).jpg",
+  "images/Season 2/(12).jpg",
+];
+List Season_3 = [
+  "images/Season 3/(1).jpg",
+  "images/Season 3/(2).jpg",
+  "images/Season 3/(3).jpg",
+  "images/Season 3/(4).jpg",
+  "images/Season 3/(5).jpg",
+  "images/Season 3/(6).jpg",
+  "images/Season 3/(7).jpg",
+  "images/Season 3/(8).jpg",
+  "images/Season 3/(9).jpg",
+  "images/Season 3/(10).jpg",
+];
+List Season_4 = [
+  "images/Season 4/(1).jpg",
+  "images/Season 4/(2).jpg",
+  "images/Season 4/(3).jpg",
+  "images/Season 4/(4).jpg",
+  "images/Season 4/(5).jpg",
+  "images/Season 4/(6).jpg",
+  "images/Season 4/(7).jpg",
+  "images/Season 4/(8).jpg",
+  "images/Season 4/(9).jpg",
+  "images/Season 4/(10).jpg",
+];
